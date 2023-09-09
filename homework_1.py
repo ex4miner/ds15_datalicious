20af89ba7718283c6ad4ddae1673413a815c0c39
ex4miner <brocia1979@gmail.com>
2023-09-09 10:23:13 +0700

    Script 09092023

---
+++ homework_1.py
@@ -0,0 +1,21 @@
+class Hewan:
+    nama_latin = "Ailuropoda melanoleuca"
+    def __int__(self, nama, umur):
+        self.nama = nama
+        self.umur = umur
+
+    def bangun(self): #instance method
+        print("Panda bernama {}, dengan usia {}")
+
+    @classmethod
+    def makan(self):
+
+    @staticmethod
+    def tidur(self):
+
+class Kucing(Hewan):
+
+class Kelinci(Hewan):
+
+
+
