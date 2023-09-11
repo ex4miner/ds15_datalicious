be9df65cadab5161be2fc77b1733bb19388c1d93
ex4miner <brocia1979@gmail.com>
2023-09-11 13:05:42 +0700

    Final Version

--- homework_1.py
+++ homework_1.py
@@ -1,8 +1,9 @@
 class Hewan:
     nama_latin = "Animalia"
-    def __init__(self, nama, umur):
+    def __init__(self, nama, umur, porsi_makan):
         self.nama = nama
         self.umur = umur
+        self.porsi_makan = porsi_makan
 
     def bangun(self): #instance method
         if self.umur > 2.5:
@@ -11,8 +12,9 @@ class Hewan:
             print("Hewan bernama {} ({} tahun) bangun di 10am".format(self.nama,self.umur))
 
     @classmethod
-    def makan(jumlah, nama, umur):
-        return jumlah(nama, umur)
+    def makan(cls, nama, umur, jumlah_pagi, jumlah_sore):
+        banyak = jumlah_pagi + jumlah_sore
+        return cls(nama, umur, banyak)
 
     @staticmethod
     def tidur(nama, umur):
@@ -24,63 +26,60 @@ class Hewan:
 class Kucing(Hewan):
     nama_latin = 'Felis Catus'
 
-    def __init__(self, nama, umur):
-        super().__init__(nama,umur)
+    def __init__(self, nama, umur, porsi_makan, kecepatan=10):
+        super().__init__(nama,umur, porsi_makan)
+        self.kecepatan = kecepatan
 
-    def bangun(self): #instance method
-        if self.umur > 2.5:
-            print("Hewan bernama {} ({} tahun) bangun di 8am".format(self.nama,self.umur))
-        else:
-            print("Hewan bernama {} ({} tahun) bangun di 10am".format(self.nama,self.umur))
-
-    @classmethod
-    def makan(jumlah, nama, umur):
-        return jumlah(nama, umur)
-
-    @staticmethod
     def tidur(nama, umur):
         if umur > 2.5:
             print("Durasi tidur hewan, {} selama 13 jam atau lebih".format(nama))
         else:
             print("Durasi tidur hewan,{} selama 20 jam".format(nama))
     
-    def lari(kecepatan=10):
-        if kecepatan > 10:
-            print("Cepat Sekali")
+    def lari(self):
+        if self.kecepatan > 10:
+            print("{} lari dengan Cepat Sekali".format(self.nama))
         else:
-            print("Lamban")
+            print("{} lari dengan Lamban".format(self.nama))
 
 class Kelinci(Hewan):
     nama_latin = 'Oryctolagus cuniculus'
 
-    def __init__(self, nama, umur):
-        super().__init__(nama,umur)
+    def __init__(self, nama, umur, porsi_makan, kecepatan=10):
+        super().__init__(nama,umur, porsi_makan)
+        self.kecepatan = kecepatan
 
-    def bangun(self): #instance method
-        if self.umur > 2.5:
-            print("Hewan bernama {} ({} tahun) bangun di 8am".format(self.nama,self.umur))
-        else:
-            print("Hewan bernama {} ({} tahun) bangun di 10am".format(self.nama,self.umur))
-
-    @classmethod
-    def makan(jumlah, nama, umur):
-        return jumlah(nama, umur)
-
-    @staticmethod
     def tidur(nama, umur):
         if umur > 2.5:
             print("Durasi tidur hewan, {} selama 14 jam atau lebih".format(nama))
         else:
             print("Durasi tidur hewan,{} selama 12 jam".format(nama))
-    
-    def lari(kecepatan=10):
-        if kecepatan > 10:
-            print("Cepat Sekali")
+
+    def lari(self):
+        if self.kecepatan > 10:
+            print("{} lari dengan Cepat Sekali".format(self.nama))
         else:
-            print("Lamban")
+            print("{} lari dengan Lamban".format(self.nama))
 
 print(Hewan.nama_latin)
-animal_1 = Hewan(nama="Panda", umur=30)
-print(animal_1.nama, animal_1.umur)
-print(animal_1.bangun)
-print(animal_1.tidur(animal_1.nama,animal_1.umur))
+animal_1 = Hewan(nama="Panda Jantan", umur=15, porsi_makan=100)
+print(animal_1.nama, animal_1.umur, animal_1.porsi_makan)
+animal_1.bangun()
+animal_1.tidur(animal_1.nama,animal_1.umur)
+
+animal_2 = Hewan.makan("Panda Betina", 10, 45, 50)
+print("Porsi makan {} per hari sebanyak {}".format(animal_2.nama, animal_2.porsi_makan))
+
+print("")
+print(Kucing.nama_latin)
+animal_3 = Kucing(nama="Cat", umur=10, porsi_makan=10,kecepatan=100)
+print(animal_3.nama, animal_3.umur, animal_3.porsi_makan)
+animal_3.bangun()
+animal_3.lari()
+
+print("")
+print(Kelinci.nama_latin)
+animal_4 = Kelinci(nama="Rabbit", umur=10, porsi_makan=10)
+print(animal_4.nama, animal_4.umur, animal_4.porsi_makan)
+animal_4.bangun()
+animal_4.lari()
